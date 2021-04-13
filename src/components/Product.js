dedc38cfd07f0b7b3e5d1cb8f7e49eb6547dd3f6
Mihir <ranemihir3@gmail.com>
2021-04-13 22:22:06 +0530

    Improved UI of Cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,19 +16,19 @@ function Product(props){
     return <Grid item lg={4} xs={12} md={6} className="grid-style">
               <Card className="card-style">
                 <Card.Img variant="top" className="card-img" src={props.product.image} />
-                <Card.Body>
+                <Card.Body style={{backgroundColor: "#70c745", color: "white"}}>
                   <Card.Title className="card-title">
                       {props.product.title}
                   </Card.Title>
                   <Card.Text className="card-text">
                      <p className="mrp-style">MRP:{props.product.price}</p>
-                     <p className="desc-style">{props.product.desc}<br/><strong>Posted on {props.product.date} by {props.product.owner}</strong><br />{props.product.city},{props.product.state}</p>
-                      <p>Contact No: {props.product.phone}</p>
+                     {/*<p className="desc-style">{props.product.desc}<br/><strong>Posted on {props.product.date} by {props.product.owner}</strong><br />{props.product.city},{props.product.state}</p>
+                      <p>Contact No: {props.product.phone}</p>*/}
                   </Card.Text>
                 </Card.Body>
-                <Card.Footer className="text-center">
+                {/*<Card.Footer className="text-center">
                   <Button size="lg" variant="success" onClick={handleClick}>Buy</Button>
-                </Card.Footer>
+                </Card.Footer>*/}
               </Card>
            </Grid>
 }
